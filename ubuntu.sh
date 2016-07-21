c3d9d752c146b6180d917c58adfdc9abe08010b1
Geraldi Karim <geraldi.karim@gmail.com>
2016-07-21 10:07:48 +0700

    update hom rvm install ruby and rails

--- ubuntu.sh
+++ ubuntu.sh
@@ -53,8 +53,10 @@ bash /tmp/npm-g-nosudo.sh
 npm -g install gulp
 
 ## Install ruby
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 
-curl -fL https://get.rvm.io | bash -s stable --ruby --rails
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -fL https://get.rvm.io | bash -s stable --ruby
+source /home/geraldi/.rvm/scripts/rvm
+gem install rails --no-rdoc --no-ri
 
 
 
