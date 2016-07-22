dad2a7cffb72e06173abab3844cc9ee724a23a0f
Geraldi Karim <geraldi.karim@gmail.com>
2016-07-22 22:00:27 +0700

    slim things down and remove configuration file settings

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,13 +2,11 @@
 
 # Set up ...
 
-alias install="sudo apt-get install -y"
-
 sudo apt-get update
 
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
-install curl software-properties-common
+sudo apt-get install -y vim curl software-properties-common
 
 
 
@@ -16,48 +14,44 @@ install curl software-properties-common
 # Server Part...
 
 ## Install zsh
-install zsh
+sudo apt-get install -y zsh
 sudo chsh -s $(which zsh) $(whoami)
 sh -c "$(curl -fL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 ## Install htop
-install htop
+sudo apt-get install -y htop
 
 ## Install git
-install git gitk
-
-## Install vim
-install vim
+sudo apt-get install -y git gitk
 
 ## Install php
 sudo add-apt-repository -y ppa:ondrej/php
 
-install php7.0-cli php7.0-dev php-mbstring \
-        php-mysql php-pgsql php-sqlite3 \
-        php7.0-mcrypt php-gd
+sudo apt-get install -y php7.0-cli php7.0-dev php-mbstring \
+                        php-mysql php-pgsql php-sqlite3 \
+                        php7.0-mcrypt php-gd
 
+## Install composer
 curl -fL https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 printf "\nPATH=\"$(composer config -g home 2>/dev/null)/vendor/bin:\$PATH\"\n" | tee -a ~/.profile
 source ~/.profile
 
-composer global require laravel/installer
-
 ## Install npm
 curl -fL https://deb.nodesource.com/setup_6.x | sudo bash -
-install nodejs
+sudo apt-get install -y nodejs
 
 curl -fL https://raw.githubusercontent.com/glenpike/npm-g_nosudo/master/npm-g-nosudo.sh > /tmp/npm-g-nosudo.sh
 bash /tmp/npm-g-nosudo.sh
 
-npm -g install gulp
+# npm -g install gulp
 
 ## Install ruby
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -fL https://get.rvm.io | bash -s stable --ruby
 source /home/geraldi/.rvm/scripts/rvm
-gem install rails --no-rdoc --no-ri
+# gem install rails --no-rdoc --no-ri
 
 
 
@@ -73,15 +67,15 @@ function install_deb() {
 ## Install tlp
 sudo add-apt-repository -y ppa:linrunner/tlp
 sudo apt-get update
-install tlp
+sudo apt-get install -y tlp
 
 ## Install indicators
-install indicator-multiload indicator-cpufreq
+sudo apt-get install -y indicator-multiload indicator-cpufreq
 
 ## Install variety
 sudo add-apt-repository -y ppa:peterlevi/ppa
 sudo apt-get update
-install variety variety-slideshow
+sudo apt-get install -y variety variety-slideshow
 
 ## Install dropbox
 install_deb dropbox https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb
@@ -89,15 +83,11 @@ install_deb dropbox https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_
 ## Install sublime text
 install_deb sublime https://download.sublimetext.com/sublime-text_build-3083_amd64.deb
 
-composer global require friendsofphp/php-cs-fixer
-composer global require phpmd/phpmd
-composer global require phpcs/phpcs
-
 ## Install valentina studio
 install_deb vstudio http://www.valentina-db.com/en/studio/download/current/vstudio_x64_lin-deb
 
 ## Install virtualbox
-install virtualbox virtualbox-guest-additions-iso
+sudo apt-get install -y virtualbox
 
 ## Install vagrant
 curl -fL https://releases.hashicorp.com/vagrant/1.8.5/vagrant_1.8.5_x86_64.deb > /tmp/vagrant.deb
@@ -115,29 +105,21 @@ cd ~
 
 # Others
 
-function echo_if_not_exist() {
-    file=$1
-    line=$2
-    grep --quiet --fixed-strings 'include "$line"' "$file" || echo "$line" >> "$file"
-}
+# function echo_if_not_exist() {
+#     file=$1
+#     line=$2
+#     grep --quiet --fixed-strings 'include "$line"' "$file" || echo "$line" >> "$file"
+# }
 
-echo_if_not_exist ~/.zshrc "alias zshconfig='vim ~/.zshrc'"
-echo_if_not_exist ~/.zshrc "alias zshsource='source ~/.zshrc'"
+# echo_if_not_exist ~/.zshrc "alias zshconfig='vim ~/.zshrc'"
+# echo_if_not_exist ~/.zshrc "alias zshsource='source ~/.zshrc'"
 
-echo_if_not_exist ~/.zshrc "alias upgrade='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get auto-remove -y'"
-echo_if_not_exist ~/.zshrc "alias install='sudo apt-get install -y'"
-echo_if_not_exist ~/.zshrc "alias remove='sudo apt-get remove -y'"
-echo_if_not_exist ~/.zshrc "alias purge='sudo apt-get purge -y'"
+# echo_if_not_exist ~/.zshrc "alias upgrade='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get auto-remove -y'"
+# echo_if_not_exist ~/.zshrc "alias install='sudo apt-get install -y'"
+# echo_if_not_exist ~/.zshrc "alias remove='sudo apt-get remove -y'"
+# echo_if_not_exist ~/.zshrc "alias purge='sudo apt-get purge -y'"
 
-echo_if_not_exist ~/.zshrc "alias hom='function __homestead() { (cd ~/Homestead && vagrant $*); unset -f __homestead; }; __homestead'"
-echo_if_not_exist ~/.zshrc "alias art='php artisan'"
+# echo_if_not_exist ~/.zshrc "alias hom='function __homestead() { (cd ~/Homestead && vagrant $*); unset -f __homestead; }; __homestead'"
+# echo_if_not_exist ~/.zshrc "alias art='php artisan'"
 
-source ~/.zshrc
-
-
-
-
-# Cleanup...
-
-unset -f echo_if_not_exist
-unset -f install_deb
+# source ~/.zshrc
